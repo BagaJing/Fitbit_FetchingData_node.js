79d8c3e66b388da6608070e6fb6f068a7fee9278
BaGaJingKang <45363787+BagaJing@users.noreply.github.com>
2019-03-06 10:15:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*
 Author : Jing Kang
 start at: 24/2/2019
-today date: 5/3/2019
-status: in progress 
+today date: 6/3/2019
+In Progress: fetching heartrate 
 
 */
 
@@ -17,7 +17,7 @@ var moment = require('moment');
 
 /*Fitbit CLient password*/
 var CLIENT_ID = '22DCGZ';
-var CLIENT_SECRET = '';
+var CLIENT_SECRET = '9a9bc5ff34992717f7d7cc1f391a4268';
 var fitbit = new FitbitApiClient({
     clientId: CLIENT_ID,
     clientSecret: CLIENT_SECRET,
@@ -26,7 +26,7 @@ var fitbit = new FitbitApiClient({
 
 /*connect database and open*/
 var mongoose = require('mongoose');
-	mongoose.connect('mongodb+srv://Jing:password@cluster0-fh0jl.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true });
+	mongoose.connect('mongodb+srv://Jing:mVWIu5idFBrQO38d@cluster0-fh0jl.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true });
 var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function (callback) {
@@ -57,18 +57,17 @@ router.get('/auth/:userId',(req, res) => {
     //res.cookie('fitbitUser', req.params.userId);
 
     res.redirect(fitbit.getAuthorizeUrl(
-        'activity heartrate location nutrition profile settings sleep social weight',config.serverURL,'login cosent'
-        )); 
+        'activity heartrate location nutrition profile settings sleep social weight',config.serverURL,'login')); 
 });
 
 router.get("/callback", (req, res) => {
 	//var userId = req.cookies.fitbitUser||-1;
 	//console.log(userId);
-	fitbit.getAccessToken(req.query.code,config.serverURL).then(result => {
+	fitbit.getAccessToken(req.query.code,config.serverURL).then(auth => {
 		// when we are here, we get the access token and user ID// then we save the access in database
 		
 		// use the access token to fetch the user's information
-	fitbit.get("/profile.json", result.access_token).then(results => {
+	fitbit.get("/profile.json", auth.access_token).then(results => {
 		//before save to database, we need to check the user subscribe or not by checking userId in database
 		var checkId= results[0].user.encodedId;
 		profileModel.findOne({'userID':checkId},function(err,doc){
@@ -80,8 +79,8 @@ router.get("/callback", (req, res) => {
 					 } else{
 					 	  // user data is not in database, then save it.
 					 	  // init the helath data as 0 and update them later.
-							var saveprofile= new profileModel({ access_token:result.access_token,
-															    refresh_token:result.refresh_token,
+							var saveprofile= new profileModel({ access_token:auth.access_token,
+															    refresh_token:auth.refresh_token,
 										   						userID:results[0].user.encodedId,
 										   						name:results[0].user.fullName,
 										   						gender:results[0].user.gender,
@@ -91,7 +90,12 @@ router.get("/callback", (req, res) => {
 							//console.log('test: '+saveprofile.DailySteps);
 								saveprofile.save(function(err){
 																if(err) return console.error(err);
-																console.log("got new user!");
+																console.log("Got new user!");
+																console.log("Current time "+getDateTime());
+																console.log('New User name: '+saveprofile.name);
+																console.log('New User ID: '+saveprofile.userID);
+																console.log('Acess Token: '+auth.access_token);
+																console.log('Refresh Token: '+auth.refresh_token);
 																});
 									res.send('Hello '+results[0].user.fullName+' !'
 					 							+'You have subscribed our service! Now you can close this page!');
@@ -131,22 +135,27 @@ var FitbitSync = function(subscr){
 	// // update activity data 
 	var _Getdata = function(subscriber){
 			//First, we try to get the data with the current access token available.
-
-		fitbit.get("/profile.json",subscriber.access_token).then(function(result){
+if (subscriber.userID=='test') {
+	console.log('Here is a test data to avoid error');
+} else{
+			fitbit.get("/profile.json",subscriber.access_token).then(function(result){
 			console.log('Checking User '+subscriber.userID+' availability of access_token');
 			// check the token first
-
+			//  
 			if ((result[0].errors && result[0].errors.length > 0 && result[0].errors[0].errorType === 'expired_token')){
 				console.log('User '+subscriber.userID+' token expired, need to be refreshed');
-				fitbit.refreshAccessToken(subscriber.access_token,subscriber.refresh_token).then(function(newtoken){
-					//console.log(newtoken);
+				fitbit.refreshAccessToken(subscriber.access_token,subscriber.refresh_token,28800).then(function(newtoken){
+					console.log(newtoken);
+					subscriber.access_token = ' ';
+					subscriber.refresh_token = ' ';
 					subscriber.access_token = newtoken.access_token;
 					subscriber.refresh_token = newtoken.refresh_token;
+					needUpdate = true;
 					needFresh = true;
 					_GetHealthData(subscriber);
-					//console.log('successfully refresh tokens!');
+					
 				},function(err){
-					console.log(subscriber.userID+'`s data is suffering a bug');
+					console.log(subscriber.userID+'`s data is  suffering a bug');
 					console.log(err.context);
 				});
 
@@ -158,6 +167,8 @@ var FitbitSync = function(subscr){
 		
 		},function(err){console.log(err)});
 	};
+}
+
 
 	// update activity data 
 	var _GetHealthData = function(subscriber){
@@ -200,6 +211,7 @@ var FitbitSync = function(subscr){
 									+' totalAsleep time has been updated to'+subscriber.DailySleep);
 					} else if (needFresh) {
 						_updateDatabase(subscriber);
+						console.log('successfully refresh tokens for User: '+subscriber.userID);
 					} else {
 						console.log('User '+subscriber.userID+' doesn`t need update');
 					}
@@ -213,10 +225,10 @@ var FitbitSync = function(subscr){
 
 		//update the data when needUpdate == true
 	var _updateDatabase = function(subscriber){
-		console.log('User: '+subscriber.userID+'updating is needed: ' + needUpdate);
+		console.log('User: '+subscriber.userID+' `s data need to be updated');
 		if (needUpdate) {
-			profileModel.updateOne({userID:subscriber.userID},{access_token:subscriber.access_token,
-     														   refresh_token:subscriber.refresh_token,
+			profileModel.updateOne({userID:subscriber.userID},{access_token: subscriber.access_token,
+															   refresh_token: subscriber.refresh_token,
      														   name:subscriber.name,
      														   gender:subscriber.gender,
      													       DailySteps:subscriber.DailySteps,
@@ -229,7 +241,6 @@ var FitbitSync = function(subscr){
 		}
 
 	};
-
 
 	this.syncProfile =function(){
 		_Getdata(checkingProfile);
@@ -269,9 +280,7 @@ function getDateTime(){
 
 }
 
-
-/* an interval function to refresh data during a certain time. currently let it run a time for half hour*/
-setInterval(function(){
+function  multiThreadingProfile() {
 	console.log(" ");
 	console.log(" ");
 	console.log("Current sync process begin at "+getDateTime());
@@ -283,7 +292,15 @@ setInterval(function(){
 			new FitbitSync(profileModel).syncProfile();
 		});
 	});
-},1800000);
+}
+
+
+
+multiThreadingProfile();
+/* an interval function to refresh data during a certain time. currently let it run a time for half hour*/
+setInterval(function(){
+	multiThreadingProfile();
+},600000);
 //1800000
 app.listen(3000);
 console.log('Server running at http:localhost:3000/');
